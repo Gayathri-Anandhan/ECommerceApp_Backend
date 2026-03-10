a5ecca3580285a4a93485aac5dbab9701aaa4738
GayathriAnandhan <gayathrianandhan2812@gmail.com>
2026-03-10 23:35:03 +0530

    Final chnages 2

--- src/main/java/com/example/ECommerce/ECommerceApplication.java
+++ src/main/java/com/example/ECommerce/ECommerceApplication.java
@@ -2,12 +2,29 @@ package com.example.ECommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ECommerceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ECommerceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ECommerceApplication.class, args);
+    }
 
+    // CORS configuration
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**") // allow all API paths
+                        .allowedOrigins("https://e-commerce-app-frontend-xi-nine.vercel.app") // your frontend URL
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // allowed HTTP methods
+                        .allowedHeaders("*") // allow all headers
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
